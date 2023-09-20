5f22ed8b52e8b35fc2e57df051feeb20727e0020
akhidevGIT <devarapalliakhila@gmail.com>
2023-09-20 21:15:02 +0100

    Commit with testthat file

--- R/addmath.R
+++ R/addmath.R
@@ -1,3 +1,15 @@
+#' add two numeric vectors of same length
+#'
+#' @param a A numeric vector of length n
+#' @param b A numeric vector of length n
+#'
+#' @return Returns a numeric vector of length n
+#'
+#' @export
+#'
+#' @examples
+#' addmath(c(1,2),c(3,4))
+#' addmath(1,2)
 addmath <- function(a,b){
   a+b
 }
